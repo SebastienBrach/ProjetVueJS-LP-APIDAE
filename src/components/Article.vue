d3d031fc605baa6cf09b11e34cbec5cd72174ba6
SebastienBrach <55393279+SebastienBrach@users.noreply.github.com>
2021-03-28 17:18:15 +0200

    Ajout de bouton pour les articles

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -5,12 +5,20 @@
       <h5 class="card-title">{{ titre }}</h5>
       <p class="card-text">{{ contenu }}</p>
       <button
-        class="btn btn-primary"
+        class="btn btn-success"
         type="submit"
         name="action"
         v-on:click="connexion"
       >
-        Connexion
+        Modifier</button
+      ><br /><br />
+      <button
+        class="btn btn-danger"
+        type="submit"
+        name="action"
+        v-on:click="connexion"
+      >
+        Supprimer
       </button>
     </div>
     <div class="card-footer"></div>
@@ -20,12 +28,28 @@
 
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Article",
   props: {
     _id: Number,
     titre: String,
     contenu: String,
+  },
+  methods: {
+    connexion() {
+      axios
+        .post(
+          "https://brach-node.herokuapp.com/login",
+          { email: this.email, password: this.password },
+          { "Content-Type": "application/x-www-form-urlencoded" }
+        )
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => console.log(error));
+    },
   },
 };
 </script>

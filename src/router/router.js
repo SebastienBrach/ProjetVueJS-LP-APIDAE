d6c8164a6791544a755216edfae1077d63ab0468
SebastienBrach <55393279+SebastienBrach@users.noreply.github.com>
2021-03-28 17:15:34 +0200

    yes

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,6 @@
 import Home from "../pages/Home";
-import Register from "../pages/Register";
-import Login from "../pages/Login";
+import Register from "../pages/RegisterForm";
+import Login from "../pages/LoginForm";
 import Article from "../pages/Articles";
 
 import axios from "axios";

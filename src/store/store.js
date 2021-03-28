4e0161fda234afd2ad1c9553b6aed24245acbc9b
SebastienBrach <55393279+SebastienBrach@users.noreply.github.com>
2021-03-28 14:46:20 +0200

    ajout d'un meilleure store je pense

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,7 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({
+/*const store = new Vuex.Store({
   state: {
     article: [
       {
@@ -23,6 +23,32 @@ const store = new Vuex.Store({
         commit("FETCH_SKILL", response.data[0].titre);
         console.log(response.data[0].titre);
       });
+    }
+  }
+});
+*/
+
+const store = new Vuex.Store({
+  state: {
+    articles: []
+  },
+  mutations: {
+    loadArticles(state) {
+      axios
+        .get("https://brach-node.herokuapp.com/article")
+        .then((response) => {
+          state.article = [];
+          for (let i = 0; i < response.data.length; i++) {
+            console.log(response.data[i]);
+          }
+        })
+        .then((response) => {
+          state.article = [];
+          for (let i = 0; i < response.data.length; i++) {
+            state.article.push(response.data[i]);
+          }
+        })
+        .catch((error) => console.log(error));
     }
   }
 });

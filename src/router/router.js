2b524e6f30ed488c02c606dcb2a71bb025e1dac7
SebastienBrach <55393279+SebastienBrach@users.noreply.github.com>
2021-03-28 14:48:33 +0200

    maj du router

--- src/router/router.js
+++ src/router/router.js
@@ -2,10 +2,6 @@ import Home from "../pages/Home";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
 import Article from "../pages/Article";
-// import ArticleInfo from "../pages/ArticleInfo";
-import DeleteArticle from "../pages/DeleteArticle";
-import UpdateArticle from "../pages/UpdateArticle";
-import AddArticle from "../pages/AddArticle";
 
 import axios from "axios";
 import Vue from "vue";
@@ -17,10 +13,7 @@ const routes = [
   { path: "/", component: Home, name: "home" },
   { path: "/register", component: Register, name: "register" },
   { path: "/login", component: Login, name: "login" },
-  { path: "/article", component: Article, name: "article" },
-  { path: "/delete/article", component: DeleteArticle, name: "deleteArticle" },
-  { path: "/update/article", component: UpdateArticle, name: "updateArticle" },
-  { path: "/add/article", component: AddArticle, name: "addArticle" }
+  { path: "/article", component: Article, name: "article" }
 ];
 
 Vue.prototype.$axios = axios;

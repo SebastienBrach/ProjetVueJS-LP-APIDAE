909940bbf81e62d11b527cf04306cf401f3b18b3
SebastienBrach <55393279+SebastienBrach@users.noreply.github.com>
2021-03-31 09:36:04 +0200

    mODIFICATION OK

---
+++ src/pages/UpdateArticleForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="listeArticle">
+    <UpdateArticle />
+  </div>
+</template>
+
+<script>
+import UpdateArticle from "../components/UpdateArticle";
+
+export default {
+  name: "UpdateArticleForm",
+  components: {
+    UpdateArticle,
+  },
+};
+</script>
